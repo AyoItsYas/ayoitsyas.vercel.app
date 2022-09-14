d4949aeed0c67c94e69687fdb5be9fd381ad5621
Yasiru Dharmathilaka <50617221+ItsYasiru@users.noreply.github.com>
2022-09-14 01:28:54 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,6 +56,7 @@ function IndexPage() {
                     rel='icon'
                     href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✨</text></svg>'
                 />
+                <meta name="google-site-verification" content="dbfHPoHZg9kxN2xrs3_bw6DOWKsM8wc0SGqxQ8FBcfw" />
             </Head>
 
             <header>
